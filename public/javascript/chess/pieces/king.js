47f5d2cb553565b59fe64133380eaeb77d9d56fa
GSRK-BZA <gsrkbza@gmail.com>
2024-09-26 17:12:58 +0530

    King code

--- public/javascript/chess/pieces/king.js
+++ public/javascript/chess/pieces/king.js
@@ -1,11 +1,33 @@
-var King = function(config){
+var King = function(config) {
     this.type = 'king';
     this.constructor(config);
 };
 
-
-
 King.prototype = new Piece({});
-King.prototype.move = function(newPosition){
 
-}
+King.prototype.isValidPosition = function(targetPosition) {
+    let currentCol = this.position.charAt(0);
+    let currentRow = parseInt(this.position.charAt(1));
+
+    let targetCol = targetPosition.col;
+    let targetRow = parseInt(targetPosition.row);
+
+    let colDiff = Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0));
+    let rowDiff = Math.abs(targetRow - currentRow);
+
+    if (colDiff <= 1 && rowDiff <= 1) {
+        return true;
+    }
+
+    console.warn("Invalid move for king");
+    return false;
+};
+
+King.prototype.moveTo = function(targetPosition) {
+    if (this.isValidPosition(targetPosition)) {
+        this.position = targetPosition.col + targetPosition.row;
+        this.render();
+    } else {
+        console.warn("Move not allowed");
+    }
+};
