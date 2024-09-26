7b9aba92c66f0157a2c289426abbbd46e68a5f0c
Tejas Ghatule <141946130+CodeMaverick2@users.noreply.github.com>
2024-09-26 18:50:19 +0000

    Alternate move functionality added(Turn Fixed)

--- public/javascript/chess/pieces/bishop.js
+++ public/javascript/chess/pieces/bishop.js
@@ -6,9 +6,10 @@ var Bishop = function(config){
 Bishop.prototype = new Piece({});
 
 Bishop.prototype.moveTo = function(newPosition){
-    if(this.isValidPosition(newPosition)){
+    if(this.isValidPosition(newPosition) && this.color === this.board.currentPlayer){
         this.position = newPosition.col + newPosition.row;
         this.render();
+        this.board.switchPlayer();
     }else{
         throw new Error("Invalid Bishop move");
     }
