5e1e1f9adce1154d084b7a347dd44e2a9ffca994
Tejas Ghatule <141946130+CodeMaverick2@users.noreply.github.com>
2024-10-26 03:43:25 +0000

    check case handled for king

--- public/javascript/chess/pieces/king.js
+++ public/javascript/chess/pieces/king.js
@@ -8,26 +8,41 @@ King.prototype = new Piece({});
 King.prototype.isValidPosition = function(targetPosition) {
     let currentCol = this.position.charAt(0);
     let currentRow = parseInt(this.position.charAt(1));
-
     let targetCol = targetPosition.col;
     let targetRow = parseInt(targetPosition.row);
-
     let colDiff = Math.abs(targetCol.charCodeAt(0) - currentCol.charCodeAt(0));
     let rowDiff = Math.abs(targetRow - currentRow);
 
     if (colDiff <= 1 && rowDiff <= 1) {
-        return true;
+        if (targetCol >= 'a' && targetCol <= 'h' && targetRow >= 1 && targetRow <= 8) {
+            const targetPiece = this.board.getPieceAt(targetPosition);
+            if (!targetPiece || targetPiece.color !== this.color) {
+                return true;
+            }
+        }
     }
-
-    console.warn("Invalid move for king");
     return false;
+};
+
+King.prototype.isSafePosition = function(targetPosition) {
+    const opponentPieces = this.board.getPiecesByColor(this.color === 'white' ? 'black' : 'white');
+    for (let piece of opponentPieces) {
+        if (piece.isValidPosition(targetPosition)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 King.prototype.moveTo = function(targetPosition) {
     if (this.isValidPosition(targetPosition) && this.color === this.board.currentPlayer) {
-        this.position = targetPosition.col + targetPosition.row;
-        this.render();
-        this.board.switchPlayer();
+        if (this.isSafePosition(targetPosition)) {
+            this.position = targetPosition.col + targetPosition.row;
+            this.render();
+            this.board.switchPlayer();
+        } else {
+            console.warn("Move not allowed: Target position is unsafe for the king");
+        }
     } else {
         console.warn("Move not allowed");
     }
